98e37305d2210bf594872b37b1055bdfbc6ab145
Matthew Lee <Mattminwoolee@Matthews-MacBook-Pro-3.local>
2018-10-21 16:59:04 -0700

    Updated icons for skills section

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -17,45 +17,44 @@ const About = () => {
           </div>
         </div>
         <div className={ styles.skillsContainer } >
-          <div><h4>Skills</h4></div>
+          <div><h4 style={{ fontWeight: "800"}}>Skills</h4></div>
           <div className={ styles.languages }>
             <h4>Languages</h4>
-            <li>JavaScript ES6</li>
-            <li>HTML5</li>
-            <li>CSS3</li>
-            <li>SQL</li>
-            <li>Java</li>
-            <li>MATLAB</li>
+            <li><i className="devicon-javascript-plain"></i> JavaScript ES6</li>
+            <li><i className="devicon-html5-plain"></i> HTML5</li>
+            <li><i className="devicon-css3-plain"></i> CSS3</li>
+            <li><i className="devicon-mysql-plain"></i> SQL</li>
+            <li><i className="devicon-java-plain"></i> Java</li>
+            <li><img src="/images/matlab.png" style={{height: "2.5vmin", width: "2.5vmin", objectFit: "contain"}}/> MATLAB</li>
           </div>
           <div className={ styles.frameworks} >
             <h4>Frameworks</h4>
-            <li>ReactJS</li>
-            <li>AngularJS</li>
-            <li>Redux</li>
-            <li>Node.js</li>
-            <li>Express</li>
-            <li>Mocha</li>
-            <li>Jest</li>
+            <li><i className="devicon-react-original"></i> ReactJS</li>
+            <li><i className="devicon-angularjs-plain"></i> AngularJS</li>
+            <li><img src="/images/redux.png" style={{height: "2.5vmin", width: "2.5vmin", objectFit: "contain"}}/> Redux</li>
+            <li><i className="devicon-nodejs-plain"></i> Node.js</li>
+            <li><i className="devicon-express-original"></i> Express</li>
+            <li><i className="devicon-mocha-plain"></i> Mocha</li>
+            {/* <li>Jest</li> */}
           </div>
           <div className={ styles.tools} >
             <h4>Tools</h4>
-            <li>AWS</li>
-            <li>Webpack</li>
-            <li>Docker</li>
-            <li>Heroku</li>
-            <li>Redis</li>
-            <li>JQuery</li>
-            <li>Babel</li>
+            <li><i className="devicon-amazonwebservices-original"></i> AWS</li>
+            <li><i className="devicon-webpack-plain"></i> Webpack</li>
+            <li><i className="devicon-docker-plain"></i> Docker</li>
+            <li><i className="devicon-heroku-original"></i> Heroku</li>
+            <li><i className="devicon-redis-plain"></i> Redis</li>
+            <li><i className="devicon-jquery-plain"></i> JQuery</li>
+            <li><i className="devicon-babel-plain"></i> Babel</li>
           </div>
           <div className={ styles.others} >
             <h4>Others</h4>
-            <li>MongoDB</li>
-            <li>PostgreSQL</li>
-            <li>MySQL</li>
-            <li>Postman</li>
-            <li>MLab</li>
-            <li>Chrome Dev Tools</li>
-            <li>Git</li>
+            <li><i className="devicon-mongodb-plain"></i> MongoDB</li>
+            <li><i className="devicon-postgresql-plain"></i> PostgreSQL</li>
+            <li><i className="devicon-mysql-plain"></i> MySQL</li>
+            <li><i className="devicon-bootstrap-plain"></i> Bootstrap</li>
+            <li><i className="devicon-chrome-plain"></i> Chrome Dev Tools</li>
+            <li><i className="devicon-git-plain"></i> Git</li>
           </div>
         </div> 
         <div className={ styles.resumeContainer }>
